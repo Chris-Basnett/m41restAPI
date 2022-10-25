294ec44995f624ce394a9f0d32d8e5a1ece5d719
Chris-Basnett <basnett_c@hotmail.co.uk>
2022-10-25 11:26:04 +0100

    add midware

--- src/user/userRouters.js
+++ src/user/userRouters.js
@@ -1,10 +1,11 @@
 const {Router} = require('express')
 const {createUser, readUsers, updateUser, deleteUser} = require('./userControllers')
+const {hashPass} = require('../midware')
 
 const userRouter = Router()
 
 userRouter.get('/readUser', readUsers)
-userRouter.post('/createUser', createUser)
+userRouter.post('/createUser', hashPass, createUser)
 userRouter.put('/updateUser', updateUser)
 userRouter.delete('/deleteUser/:username', deleteUser)
 
